a6aae60b053b8b9750d67f9f1f807c6f95198390
jigallego@teco.com.ar <jigallego@teco.com.ar>
2023-07-10 12:21:37 -0300

    end section 11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,51 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+import { customTheme, lightTheme, darkTheme } from '@/themes'
+import { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props extends AppProps {
+  theme: string
 }
+export default function App({ Component, pageProps, theme = 'dark' }: Props) {
+
+  // console.log({theme})
+
+  const [currentTheme, setCurrentTheme] = useState(lightTheme)
+
+  useEffect(() => {
+
+    const cookieTheme = Cookies.get('theme') || 'light'
+    const selectedTheme = cookieTheme === 'light'
+      ? lightTheme
+      : (cookieTheme === 'dark')
+        ? darkTheme
+        : customTheme
+
+    setCurrentTheme(selectedTheme)
+  }, [])
+
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+// getInitialProps is not really recommended to use it cuz we lose the 
+// automatic static optimization and we lose the capability to use static paths
+// App.getInitialProps = async (appContext: AppContext) => {
+
+//   const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : {theme: 'light'}
+//   const validTheme= ['light', 'dark', 'custom']
+
+//   return {
+//     theme: validTheme.includes(theme) ? theme : 'dark',
+//   }  
+// }
+
+
